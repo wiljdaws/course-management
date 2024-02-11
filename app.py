8a4ed56c8f9ac47730f001103ab4748709c42d09
wiljdaws <williamsjdawson@gmail.com>
2024-02-11 16:41:41 -0600

    formatted to PEP 8 style guide

--- app.py
+++ app.py
@@ -6,13 +6,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+        f"sqlite:///{os.path.join(basedir, 'test.db')}"
+        )
 db = SQLAlchemy(app)
+
 
 class Course(db.Model):
     '''
     Create a table named Course
-    
     ---
     - Columns:
         - id (int): The id of the course.
@@ -27,22 +29,26 @@ class Course(db.Model):
     professor = db.Column(db.String(50), nullable=False)
     semester = db.Column(db.String(50), nullable=False)
 
+
 with app.app_context():
     '''
     Create the table named Course if it doesn't exist.
     '''
     db.create_all()
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     '''
-    This function will display the courses and a form to add a new course.
-    
+    This function will display the courses
+    and a form to add a new course.
     ---
     - GET:
-        This function will display the courses and a form to add a new course.
+        This function will display the courses
+        and a form to add a new course.
     - POST:
-        This function will add a new course to the database and redirect to the index page.     
+        This function will add a new course
+        to the database and redirect to the index page.
     '''
     if request.method == 'POST':
         name = request.form.get('name')
@@ -51,7 +57,8 @@ def index():
         semester = request.form.get('semester')
 
         if name and description and professor and semester:
-            course = Course(name=name, description=description, professor=professor, semester=semester)
+            course = Course(name=name, description=description,
+                            professor=professor, semester=semester)
             db.session.add(course)
             db.session.commit()
 
@@ -59,19 +66,20 @@ def index():
     else:
         courses = Course.query.all()
         return render_template('index.html', courses=courses)
-    
+
+
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit(id):
     '''
     This function will edit a course.
-    
     ---
     - Args:
        - id (int): The id of the course to edit.
     - GET:
        - This function will display the course to edit.
     - POST:
-       - This function will update the course and redirect to the index page.
+       - This function will update the course
+         in the database and redirect to the index page.
     '''
     course = Course.query.get(id)
     if course is None:
@@ -85,18 +93,19 @@ def edit(id):
         return redirect(url_for('index'))
     return render_template('edit.html', course=course)
 
+
 @app.route('/delete/<int:id>', methods=['GET', 'POST'])
 def delete(id):
     '''
     This function will delete a course.
-    
     ---
     - Args:
        - id (int): The id of the course to delete.
     - GET:
          - This function will display the course to delete.
     - POST:
-        - This function will delete the course and redirect to the index page.
+        - This function will delete the course
+          and redirect to the index page.
     '''
     course = Course.query.get(id)
     if course is None:
@@ -106,6 +115,7 @@ def delete(id):
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('delete.html', course=course)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
